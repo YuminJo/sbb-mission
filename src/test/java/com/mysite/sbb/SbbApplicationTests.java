d484304d1603dbaa262058eb55902e830fc55119
YuminJo <nationmilo@gmail.com>
2023-11-22 11:22:16 +0900

    TEST Maintaining DB transactions

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import jakarta.transaction.Transactional;
+
 @SpringBootTest
 class SbbApplicationTests {
 
@@ -19,17 +21,16 @@ class SbbApplicationTests {
 	@Autowired
 	private AnswerRepository answerRepository;
 
+	@Transactional
 	@Test
 	void testJpa() {
 		Optional<Question> oq = this.questionRepository.findById(2);
-
 		assertTrue(oq.isPresent());
 		Question q = oq.get();
 
-		Answer a = new Answer();
-		a.setContent("네 자동으로 생성됩니다.");
-		a.setQuestion(q);
-		a.setCreateDate(LocalDateTime.now());
-		this.answerRepository.save(a);
+		List<Answer> answerList = q.getAnswerList();
+
+		assertEquals(1,answerList.size());
+		assertEquals("네 자동으로 생성됩니다.",answerList.get(0).getContent());
 	}
 }
