b3c0ddc52ee78403e5dd55528757acae864f1799
YuminJo <nationmilo@gmail.com>
2023-11-22 15:37:59 +0900

    mock mvc detail test

--- src/test/java/com/mysite/sbb/question/QuestionControllerTests.java
+++ src/test/java/com/mysite/sbb/question/QuestionControllerTests.java
@@ -1,6 +1,10 @@
 package com.mysite.sbb.question;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,13 +16,15 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import jakarta.transaction.Transactional;
+
 @WebMvcTest(QuestionController.class)
 public class QuestionControllerTests {
 	@Autowired
 	private MockMvc mockMvc;
 
 	@MockBean
-	private QuestionRepository questionRepository;
+	private QuestionService questionService;
 
 	@Test
 	@DisplayName("/question/list 접속")
@@ -27,6 +33,27 @@ public class QuestionControllerTests {
 				.get("/question/list")
 				.accept(MediaType.APPLICATION_JSON))
 			.andDo(print())
-			.andExpect(MockMvcResultMatchers.status().isOk());
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andReturn();
+	}
+	@Test
+	@DisplayName("/question/detail/{id} 접속")
+	public void connect_QuestionDetail() throws Exception {
+		// 가짜 Question 객체 생성
+		Question fakeQuestion = new Question();
+		fakeQuestion.setId(2);
+		fakeQuestion.setSubject("Fake Subject");
+		fakeQuestion.setContent("Fake Content");
+
+		// QuestionService의 동작 설정
+		when(questionService.getQuestion(2)).thenReturn(fakeQuestion);
+
+		mockMvc.perform(MockMvcRequestBuilders
+				.get("/question/detail/2")
+				.accept(MediaType.APPLICATION_JSON))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.view().name("question_detail"))
+			.andExpect(MockMvcResultMatchers.model().attributeExists("question"))
+			.andExpect(MockMvcResultMatchers.model().attribute("question", fakeQuestion));
 	}
 }
