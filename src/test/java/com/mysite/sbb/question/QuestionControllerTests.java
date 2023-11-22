efe2c999a59ed6d2e90337454dbe91f22656be18
YuminJo <nationmilo@gmail.com>
2023-11-22 14:23:25 +0900

    mock mvc mockbean added

--- src/test/java/com/mysite/sbb/question/QuestionControllerTests.java
+++ src/test/java/com/mysite/sbb/question/QuestionControllerTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -17,12 +17,15 @@ public class QuestionControllerTests {
 	@Autowired
 	private MockMvc mockMvc;
 
+	@MockBean
+	private QuestionRepository questionRepository;
+
 	@Test
 	@DisplayName("/question/list 접속")
 	public void connect_QuestionList() throws Exception{
 		mockMvc.perform(MockMvcRequestBuilders
-			.get("/question/list")
-			.accept(MediaType.APPLICATION_JSON))
+				.get("/question/list")
+				.accept(MediaType.APPLICATION_JSON))
 			.andDo(print())
 			.andExpect(MockMvcResultMatchers.status().isOk());
 	}
